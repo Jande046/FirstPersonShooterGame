95eba8944ea3693b029fcd1fe877a67d21ddbdfb
Jayden Anderson <130862283+Jande046@users.noreply.github.com>
2024-11-15 16:45:18 -0500

    more UI
    health

--- SuperBowl/Assets/Editor/InteractableEditor.cs
+++ SuperBowl/Assets/Editor/InteractableEditor.cs
@@ -9,23 +9,29 @@ public class InteractableEditor : Editor
         Interactable interactable = (Interactable)target;
         if(target.GetType() == typeof(EventOnlyInteractable))
         {
-
+            interactable.promptMessage = EditorGUILayout.TextField("Prompt Message", interactable.promptMessage);
+            EditorGUILayout.HelpBox("EventOnly Interact can only use Unityevents", MessageType.Info);
+            if(interactable.GetComponent<InteractionEvent>()  == null)
+            {
+                interactable.useEvents = true;
+                interactable.GetComponent<InteractionEvent>();
+            }
         }
         else
         {
-
-         base.OnInspectorGUI();
+            base.OnInspectorGUI();
             if(interactable.useEvents)
-        {
-            if(interactable.GetComponent<InteractionEvent>() == null)
+            {
+                if(interactable.GetComponent<InteractionEvent>() == null)
                 interactable.gameObject.AddComponent<InteractionEvent>();
-        }
-        else
-        {
-            if(interactable.GetComponent<InteractionEvent>() != null)
-                DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+            }
+            else
+            {
 
-        }
+                if(interactable.GetComponent<InteractionEvent>() != null)
+                    DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+
+            }
         }
     }
 }
